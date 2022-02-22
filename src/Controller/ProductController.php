1ef8f621275aaefd74b9540a697fd97338c25115
soufcod3 <soufwra@gmail.com>
2022-02-22 15:07:15 +0100

    renaming route

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/product")
+ * @Route("/products")
  */
 class ProductController extends AbstractController
 {
