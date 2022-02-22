5b39c3f6815a2ad7d3ca029f5e284117ce933207
soufcod3 <soufwra@gmail.com>
2022-02-22 20:35:47 +0100

    scss files updated

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+import './styles/custom.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
 
 // start the Stimulus application
 import './bootstrap';
