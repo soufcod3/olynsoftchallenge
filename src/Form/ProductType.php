6871c54f3b300f1a37f1c4538259b009fd8f3b17
soufcod3 <soufwra@gmail.com>
2022-02-22 20:36:58 +0100

    form update

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,18 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('date')
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Entrez un nom',
+                ]
+            ])
+            ->add('price', IntegerType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Entrez un prix',
+                ]
+            ])
         ;
     }
 
